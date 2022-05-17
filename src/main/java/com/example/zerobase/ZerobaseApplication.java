cdb600cde9c713bea1ace4afa7e2eda52d7e0de2
soohyunan <shahn2525@gmail.com>
2022-05-17 08:18:58 +0200

    first commit

---
+++ src/main/java/com/example/zerobase/ZerobaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.zerobase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerobaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerobaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zerobase/ZerobaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zerobase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerobaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
